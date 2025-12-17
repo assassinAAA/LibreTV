a61bbd1b2e4292b91a8d11d3a9deda28f73f6cc7
Ztllll <33171642+assassinAAA@users.noreply.github.com>
2025-12-17 21:16:32 +0800

    内置API

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,22 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+    mt: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台',
+    }
+    jisu: {
+        api: 'https://jszyapi.com/api.php/provide/vod',
+        name: '极速',
+    }
+    heiliao: {
+        api: 'https://www.heiliaozyapi.com/api.php/provide/vod',
+        name: '黑料',
+    }
+    xj: {
+        api: 'https://www.xiangjiaozyw.com/api.php/provide/vod/',
+        name: '香蕉',
+    }
 };
 
 // 调用全局方法合并
